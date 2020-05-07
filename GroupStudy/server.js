7f5062d0a517a55e84acf3a540da770a2be7265a
danielleal04 <daniel.leal04@gmail.com>
2020-05-07 04:31:25 -0500

    Fixed Login and Other Pages

--- GroupStudy/server.js
+++ GroupStudy/server.js
@@ -37,17 +37,17 @@ app.use(express.static(path.join(__dirname, 'static')));
 //////////////////////////////////////////////////////////////////
 
 app.get('/', (request, response) => {
-  response.render("user/start"); 
+  response.render("user/start");
 });
 
 //////////////////////////////////////////////////////////////////
 
 app.get('/english', (request, response) => {
-  response.render("user/english_instructions"); 
+  response.render("user/english_instructions");
 });
 
 app.get('/english/get_started', (request, response) => {
-  response.render("user/english_get_started"); 
+  response.render("user/english_get_started");
 });
 
 app.post('/english/get_started/submit', (request, response) => {
@@ -55,15 +55,15 @@ app.post('/english/get_started/submit', (request, response) => {
   fullName = request.body.fullName
   phoneNumber = request.body.phoneNumber
 
-  nameEmpty = false 
-  phoneEmpty = false 
+  nameEmpty = false
+  phoneEmpty = false
 
   if (fullName.trim().length == 0) {
-    nameEmpty = true 
-  } 
+    nameEmpty = true
+  }
 
   if (phoneNumber.trim().length == 0) {
-    phoneEmpty = true 
+    phoneEmpty = true
   }
 
   if (nameEmpty || phoneEmpty) {
@@ -76,11 +76,11 @@ app.post('/english/get_started/submit', (request, response) => {
     phone_number: phoneNumber,
     status: 0,
   }).then(queue => {
-    
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write("it has been posted");  
-    response.end();     
-    
+
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write("it has been posted");
+    response.end();
+
   });
 
 });
@@ -89,26 +89,27 @@ app.post('/english/get_started/submit', (request, response) => {
 //////////////////////////////////////////////////////////////////
 
 app.get('/spanish', (request, response) => {
-  response.render("user/spanish_instructions"); 
+  response.render("user/spanish_instructions");
 });
 
 app.get('/spanish/empezar', (request, response) => {
-  response.render("user/spanish_empezar"); 
+  response.render("user/spanish_empezar");
+});
 
 app.post('/spanish/empezar/completar', (request, response) => {
 
   fullName = request.body.fullName
   phoneNumber = request.body.phoneNumber
 
-  nameEmpty = false 
-  phoneEmpty = false 
+  nameEmpty = false
+  phoneEmpty = false
 
   if (fullName.trim().length == 0) {
-    nameEmpty = true 
-  } 
+    nameEmpty = true
+  }
 
   if (phoneNumber.trim().length == 0) {
-    phoneEmpty = true 
+    phoneEmpty = true
   }
 
   if (nameEmpty || phoneEmpty) {
@@ -121,17 +122,17 @@ app.post('/spanish/empezar/completar', (request, response) => {
     phone_number: phoneNumber,
     status: 0,
   }).then(queue => {
-    
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write("it has been posted in spanish");  
-    response.end();     
-    
+
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write("it has been posted in spanish");
+    response.end();
+
   });
 
 });
 
 
-// Start Socket 
+// Start Socket
 
 
 //////////////////////////////////////////////////////////////////
@@ -140,28 +141,38 @@ app.get('/admin', (request, response) => {
   response.render("admin/dashboard");
 });
 
-//////////////////////////////////////////////////////////////////
-
-app.get('/admin/pending', (request, response) => {
-  response.render("admin/dashboard");
-});
 
 //////////////////////////////////////////////////////////////////
 
 app.get('/admin/waiting', (request, response) => {
-  response.render("admin/dashboard");
+
+  Queue.findAll({
+    where: {
+      status: 0, 
+    }
+  }).then(queue => {
+    response.render("admin/waiting", { queue: queue }); 
 });
+});
+
 
 //////////////////////////////////////////////////////////////////
 
 app.get('/admin/cutting', (request, response) => {
-  response.render("admin/dashboard");
+
+  Queue.findAll({
+    where: {
+      status: 1, 
+    }
+  }).then(queue => {
+    response.render("admin/cutting", { queue: queue }); 
+  });
 });
 
 //////////////////////////////////////////////////////////////////
 
 app.get('/admin/account', (request, response) => {
-  response.render("admin/dashboard");
+  response.render("admin/account");
 });
 
 //////////////////////////////////////////////////////////////////
@@ -170,8 +181,88 @@ app.get('/login', (request, response) => {
   response.render("admin/login");
 });
 
+app.get('admin/logoff', (request,response) => {
+	// remove user from session
+	response.redirect("admin/login");
+});
+
+app.post('/action', (request,response) => {
+
+  // set username and password
+  password = request.body.password;
+
+  // route to send user
+  var route = "";
+  if (req.body.login != undefined) route = "loginPage";
+  else route = "registerPage";
+
+
+// user pressed login button
+if (typeof req.body.login !== 'undefined') {
+
+    // attempt to login user
+
+    // get user by username
+    User.findOne({
+      where:{username: uname}
+    }).then(user => {
+      //check Password
+    if(user){
+      bcrypt.compare(pw, user.password_hash, (err,match) => {
+        if(match){
+          // add user to session, redirect to home page and check if normal or admin
+          if (user){
+            account = true;
+          }
+          req.session.user = user;
+          //res.render('allSessionsPage', {user: user, hasaccount: account});
+          res.redirect('/allSessions');
+        }
+        else{
+          res.render("/login");
+        }
+      });
+    }
+
+    });
+
+} else {
+    // attempt to register user
+
+
+  // Look up username
+  User.findOne({
+    where:{username: uname}
+  }).then(user => {
+    //check if user already exists in database
+    if(user){
+      errors.push({msg: "Username already exists!"});
+      res.render("registerPage", {errors: errors, username: uname, email: email});
+    }
+    else{
+      // if no user exists, then create user
+      User.create({
+        username: uname,
+        email: email,
+        password_hash: bcrypt.hashSync(pw, 10)
+      }).then(user => {
+        //add new user to session and check if account is normal or admin
+        if (user){
+          account = true;
+        }
+        req.session.user = user;
+        //res.render('allSessionsPage', {user: user, hasaccount: account});
+        res.redirect('/allSessions');
+      });
+
+    }
+  });
+
+}
+
+});
 
 //////////////////////////////////////////////////////////////////
 var server = app.listen(app.get('port'), function () {
-  console.log("Server started...")
+  console.log("Server started...");
 });
